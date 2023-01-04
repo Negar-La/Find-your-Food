de11ed07773f1ce4d81d465c96b3e53e2fa35bc9
Negar Lashgari <negar.lashgari@gmail.com>
2023-01-04 17:56:29 -0500

    creating the project

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const helmet = require("helmet");
+const morgan = require('morgan');
+
+const port = 8000;
+
+express()
+
+    .use(express.json())
+    .use(helmet())
+    .use(morgan('tiny'))
+
+    .get('/hello', (req, res) => {
+    res.status(200).json({status: 200, message: "hello world"}) 
+    })
+
+    .listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+    });
