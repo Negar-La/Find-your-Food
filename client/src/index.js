b4f4fa7afee7ecd8b01c8e3ada8e5c22c080ef13
Negar Lashgari <negar.lashgari@gmail.com>
2023-01-19 17:37:37 -0500

    auth0

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,15 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import { MenuProvider } from './components/MenuContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const domain= process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId= process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 root.render(
     // <React.StrictMode>
       <MenuProvider>
        <Auth0Provider
-        domain="dev-xhs6s7vmewmk7kug.us.auth0.com"
-        clientId="6lE73KYJrCIPT9y5n4dFxspGNFNkPQ1s"
+        domain={domain}
+        clientId={clientId}
         redirectUri={window.location.origin}
       >
         <App />
