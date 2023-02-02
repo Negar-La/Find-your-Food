9488bc5d382c26bc8aa6003150b8a11b168c53dc
Negar Lashgari <negar.lashgari@gmail.com>
2023-02-01 19:20:02 -0500

    deploy

--- client/src/components/Pages/HomePage.js
+++ client/src/components/Pages/HomePage.js
@@ -12,13 +12,15 @@ const HomePage = () => {
   const [posts, setPosts] = useState(null);
   const [status, setStatus] = useState("loading");
 
+  const URL = process.env.NODE_ENV !== 'development' ? process.env.REACT_APP_SERVER_URL : "";
+console.log(process.env);
   //add to favorite and remove from favorite list
 //isToggled is an object including all post.id(s) which are either true (added to favorite list) or false (removed from favorite list).
   useEffect(()=>{
-      fetch (`${process.env.REACT_APP_SERVER_URL}/api/getPosts`)
+      fetch (`${URL}/api/getPosts`)
         .then(res=> res.json())
         .then((data)=>{
-          // console.log(data);
+          console.log(data.data);
           setPosts(data.data);
         })
         .catch ((error)=>{
